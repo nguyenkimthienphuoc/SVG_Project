0575735147abad0ed6c29095a90554db4e539144
SANG NGUYỄN LÊ <24127231@student.hcmus.edu.vn>
2025-06-18 19:56:19 +0700

    svgText

--- SVGDemo/SVGText.cpp
+++ SVGDemo/SVGText.cpp
@@ -1,1 +1,39 @@
 #include "stdafx.h"
+#include "SVGText.h"
+
+SVGText::SVGText(const std::wstring& content, float x, float y, const PaintStyle& style, float fontSize)
+    : text(content), x(x), y(y), fontSize(fontSize) {
+    this->style = style;
+}
+
+void SVGText::setPosition(float x, float y) {
+    this->x = x;
+    this->y = y;
+}
+
+void SVGText::setText(const std::wstring& content) {
+    text = content;
+}
+
+void SVGText::setFontSize(float size) {
+    fontSize = size;
+}
+
+std::wstring SVGText::getText() const {
+    return text;
+}
+
+void SVGText::draw(Graphics* graphics) const {
+    SolidBrush brush(style.fillColor);
+
+    FontFamily fontFamily(L"Arial");
+    Font font(&fontFamily, fontSize, FontStyleRegular, UnitPixel);
+
+    graphics->DrawString(
+        text.c_str(),
+        -1,
+        &font,
+        PointF(x, y),
+        &brush
+    );
+}
--- SVGDemo/SVGText.h
+++ SVGDemo/SVGText.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "SVGElement.h"
+#include <string>
+
+class SVGText : public SVGElement {
+private:
+    std::wstring text;  // Text content
+    float x, y;         // Start Point
+    float fontSize;     // size
+
+public:
+    SVGText(const std::wstring& content, float x, float y, const PaintStyle& style, float fontSize);
+
+    void setPosition(float x, float y);
+    void setText(const std::wstring& content);
+    void setFontSize(float size);
+
+    std::wstring getText() const;
+
+    void draw(Graphics* graphics) const override;
+};
